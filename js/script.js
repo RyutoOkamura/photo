490c29cbdae0bdaf03f25e142fb593636987762c
RyutoOkamura <86783195+RyutoOkamura@users.noreply.github.com>
2022-05-17 23:20:44 +0900

    second-commit

--- js/script.js
+++ js/script.js
@@ -1,48 +1,55 @@
-"use strict";
-
-// Funcion que activa el overlay. Se pasa como parametro el contenedor padre (#vintalight en el DOM)
-var activeVintalight = function activeVintalight(container) {
-    // Delegacion de eventos para detectar click en los hijos
-    container.addEventListener("click", function (e) {
-        var element = e.target;
-        // Validar que se haya dado click en el pseudoelemento before
-        if (element.tagName == "DIV") {
-            // Obtener direccion y descripcion de la imagen que se dio click
-            var src = element.querySelector("img").getAttribute("src"),
-                descrip = element.querySelector("img").getAttribute("alt"),
-
-            // Crear un nuevo div que se usara como overlay
-            vintalightOverlay = document.createElement("div");
-            // Agregar clase al div que creamos para poder darle estilos con CSS
-            vintalightOverlay.classList.add("vintalight-overlay");
-            // Agregar contenido al overlay 
-            vintalightOverlay.innerHTML = "\n                <figure class=\"vintalight__container active\">\n                    <div class=\"vintalight__photo\">\n                        <img src=\"" + src + "\" alt=\"" + descrip + "\" class=\"vintalight__img\"/>\n                    </div>\n                    <figcaption class=\"vintalight__caption\">\n                        <h3 class=\"vintalight__text\">" + descrip + "</h3>\n                    </figcaption>\n                    <button class=\"vintalight__button\" id=\"button-close\">\u2715</button>\n                </figure>\n            ";
-            // Meter el overlay en el DOM
-            document.body.appendChild(vintalightOverlay);
-            // Anadimos la clase active para poder darle transicion
-            setTimeout(function () {
-                vintalightOverlay.classList.add("active");
-            }, 1);
-            // Eliminar el scroll del body
-            document.body.style.overflow = "hidden";
-            // Evento para cerrar el overlay
-            document.getElementById("button-close").addEventListener("click", function () {
-                // Eliminar clase active
-                vintalightOverlay.classList.remove("active");
-                // Eliminar overlay del DOM
-                setTimeout(function () {
-                    document.body.removeChild(vintalightOverlay);
-                }, 500);
-                // Devolver scroll al body
-                document.body.style.overflow = "auto";
-            });
-            // Evento para cerrar el overlay con la tecla ESC
-            window.addEventListener("keyup", function (e) {
-                if (e.key === "Escape") document.getElementById("button-close").click();
-            });
-        }
-    });
+// Función que activa el overlay. Se pasa como parámetro el contenedor padre (#vintalight en el DOM)
+const activeVintalight = container => {
+  // Delegación de eventos para detectar click en los hijos
+  container.addEventListener("click", e => {
+    let element = e.target;
+    // Validar que se haya dado click en el pseudoelemento before
+    if (element.tagName == "DIV") {
+      // Obtener dirección y descripción de la imagen que se dio click
+      let src = element.querySelector("img").src,
+      descrip = element.querySelector("img").alt,
+      // Crear un nuevo div que se usará como overlay
+      vintalightOverlay = document.createElement("div");
+      // Agregar clase al div que creamos para poder darle estilos con CSS
+      vintalightOverlay.classList.add("vintalight-overlay");
+      // Agregar contenido al overlay 
+      vintalightOverlay.innerHTML = `
+                <figure class="vintalight__container active">
+                    <div class="vintalight__photo">
+                        <img src="${src}" alt="${descrip}" class="vintalight__img"/>
+                    </div>
+                    <figcaption class="vintalight__caption">
+                        <h3 class="vintalight__text">${descrip}</h3>
+                    </figcaption>
+                    <button class="vintalight__button" id="button-close">✕</button>
+                </figure>
+            `;
+      // Meter el overlay en el DOM
+      document.body.appendChild(vintalightOverlay);
+      // Añadimos la clase active para poder darle transición
+      setTimeout(() => {
+        vintalightOverlay.classList.add("active");
+      }, 1);
+      // Eliminar el scroll del body
+      document.body.style.overflow = "hidden";
+      // Evento para cerrar el overlay
+      document.getElementById("button-close").addEventListener("click", () => {
+        // Eliminar clase active
+        vintalightOverlay.classList.remove("active");
+        // Eliminar overlay del DOM
+        setTimeout(() => {
+          document.body.removeChild(vintalightOverlay);
+        }, 500);
+        // Devolver scroll al body
+        document.body.style.overflow = "auto";
+      });
+      // Evento para cerrar el overlay con la tecla ESC
+      window.addEventListener("keyup", e => {
+        if (e.key === "Escape") document.getElementById("button-close").click();
+      });
+    }
+  });
 };
 
-// Activamos la funcion
+// Activamos la función
 window.addEventListener("load", activeVintalight(document.getElementById("vintalight")));
